df32422ec0382c5d01d74962bfbdcf7a0aa17df1
Ronit Raj <ronitrajofficial7@gmail.com>
2024-04-03 14:59:04 +0530

    prog: init Hono and prisma

---
+++ backend/src/index.ts
@@ -0,0 +1,31 @@
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+const app = new Hono();
+
+app.post("/api/v1/user/signup", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.post("/api/v1/user/signin", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.post("/api/v1/blog", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.put("/api/v1/blog", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get("/api/v1/blog/:id", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get("/api/v1/blog/bulk", (c) => {
+  return c.text("Hello Hono!");
+});
+
+export default app;
